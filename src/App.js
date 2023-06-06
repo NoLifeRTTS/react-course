678b1eb8a2775e924dcccf50de144a9aa9bbec41
Aleksander Shevelev <alex.shevelev.01@mail.ru>
2023-06-06 16:18:22 +0700

    add bind and onChange input

--- src/App.js
+++ src/App.js
@@ -13,11 +13,7 @@ class App extends Component {
         pageTitle: 'React Components'
     }
 
-    changeTitleHandler = () => {
-
-        const oldTitle = this.state.pageTitle;
-
-        const newTitle = oldTitle + ' (changed)';
+    changeTitleHandler = newTitle => {
 
         this.setState({
             pageTitle: newTitle
@@ -25,8 +21,14 @@ class App extends Component {
 
     }
 
+    handleInput = event => {
+        this.setState({
+            pageTitle: event.target.value
+        })
+    }
+
     render() {
-        
+
         const divStyle = {
             textAlign: 'center'
         }
@@ -37,11 +39,25 @@ class App extends Component {
             <div style={divStyle}>
                 <h1>{this.state.pageTitle}</h1>
 
-                <button onClick={this.changeTitleHandler}>Change title</button>
+                <input type="text" onChange={this.handleInput}/>
 
-                <Car name={cars[0].name} year={cars[0].year} />
-                <Car name={cars[1].name} year={cars[1].year} />
-                <Car name={cars[2].name} year={cars[2].year} />
+                <button onClick={this.changeTitleHandler.bind(this, 'Changed!')}>Change title</button>
+
+                <Car
+                    name={cars[0].name}
+                    year={cars[0].year}
+                    onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
+                />
+                <Car
+                    name={cars[1].name}
+                    year={cars[1].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
+                />
+                <Car
+                    name={cars[2].name}
+                    year={cars[2].year}
+                    onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+                />
             </div>
         );
     }
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -5,6 +5,7 @@ function Car(props) {
         <div>
             <h3>Car name: {props.name}</h3>
             <p>Year: <strong>{props.year}</strong></p>
+            <button onClick={props.onChangeTitle}>Click</button>
         </div>
     )
 }
