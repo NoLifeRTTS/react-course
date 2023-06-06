9162c781cb0602977859f3c3716ba2ac43d050e5
Aleksander Shevelev <alex.shevelev.01@mail.ru>
2023-06-06 15:45:52 +0700

    init state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
 import './App.css';
 import Car from "./Car/Car";
 
-function App() {
-    const divStyle = {
-        textAlign: 'center'
-    }
-    return (
-      <div style={divStyle}>
-        <h1>Hello world!!</h1>
-          <Car name='Ford Focus' year={2017} />
-          <Car name='Audi A8' year={2015} >
-              <p>COLOR</p>
-          </Car>
-          <Car name='Mazda 3' year={2010} />
-      </div>
-    );
+class App extends Component {
 
+    state = {
+        cars: [
+            {name: 'Ford Focus', year: 2017},
+            {name: 'Audi A8', year: 2015},
+            {name: 'Mazda 3', year: 2010}
+        ],
+        pageTitle: 'React Components'
+    }
+    render() {
+        const divStyle = {
+            textAlign: 'center'
+        }
+
+        const cars = this.state.cars
+
+        return (
+            <div style={divStyle}>
+                <h1>{this.state.pageTitle}</h1>
+
+                <Car name={cars[0].name} year={cars[0].year} />
+                <Car name={cars[1].name} year={cars[1].year} />
+                <Car name={cars[2].name} year={cars[2].year} />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -5,7 +5,6 @@ function Car(props) {
         <div>
             <h3>Car name: {props.name}</h3>
             <p>Year: <strong>{props.year}</strong></p>
-            { props.children }
         </div>
     )
 }
